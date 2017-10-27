bc6bb80b5b9e736ac5564e34760f7f2927dd0499
Mario Month <mkmonth@outlook.com>
2017-10-27 16:03:02 -0400

    Implemented bcrypt

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const VolOp = require('./volOpModel');
+const bcrypt = require('bcrypt');
 
 // create user schema and model
 const UserSchema = new Schema({
@@ -17,6 +18,30 @@ const UserSchema = new Schema({
     savedVolOps: [String] // store id of volOp
 });
 
-const User = mongoose.model('user', UserSchema);
+// Compare the passed password with the value in the database
+UserSchema.methods.comparePassword = function comparePassword(pwHash, callback) {
+    bcrypt.compare(pwHash, this.pwHash, callback);
+};
 
-module.exports = User;
+// Generate hash
+UserSchema.pre('save', function saveHook(next) {
+    const user = this;
+
+    // used only if the password is modified or the user is new
+    if(!user.isModified('pwHash')) return next();
+
+    return bcrypt.genSalt((saltError, salt) => {
+        if(saltError){ return next(saltError); }
+
+        return bcrypt.hash(user.pwHash, salt, (hashError, hash) => {
+            if(hashError){ return next(hashError); }
+
+            //replace the password with hash value
+            user.pwHash = hash;
+
+            return next();
+        });
+    });
+});
+
+module.exports = mongoose.model('User', UserSchema);
