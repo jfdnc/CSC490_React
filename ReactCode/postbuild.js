24996c70cb487204f94513c16b0045d7572272c6
Jacob Durham <jfdurham@uncg.edu>
2017-11-08 15:32:24 -0500

    updated postbuild script

--- ReactCode/postbuild.js
+++ ReactCode/postbuild.js
@@ -1,4 +1,9 @@
-var mv = require('mv')
+const mv = require('mv')
+const del = require('del')
 
-mv('./build/static/','../ServerCode/public/static/',{mkdirp:true},function(err){console.log(err)})
-mv('./build/index.html','../ServerCode/public/index.html',{mkdirp:true},function(err){console.log(err)})
+del(['../ServerCode/public/static', '../ServerCode/public/index.html'], {force:true}).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+    mv('./build/static/','../ServerCode/public/static/',{mkdirp:true},function(err){console.log(err)})
+    mv('./build/index.html','../ServerCode/public/index.html',{mkdirp:true},function(err){console.log(err)})
+
+});
