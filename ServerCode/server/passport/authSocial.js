5996c8c5512db4139e688f412b84ae3ff04fbde6
brandonsoccer22 <brandonsoccer22@gmail.com>
2017-11-21 02:37:14 -0500

    added global twitter login

--- ServerCode/server/passport/authSocial.js
+++ ServerCode/server/passport/authSocial.js
@@ -6,11 +6,25 @@ module.exports = {
         'callbackURL'   : 'http://localhost:3000/auth/facebookLogin/callback'
     },
 
+    //comment this out if you change to global
+    //Start local
     'twitterAuth' : {
         'consumerKey'       : 'K596wxqlVxEI61qHNzJhtWvwA',
         'consumerSecret'    : '9cxfKEJUFrj0rCbbeHevOuI20GD6119bSegQozZUKQPKj1n0y7',
         'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
+    //end local
+
+    //uncomment to use global, then comment out local
+    //start global
+    /*
+    'twitterAuth' : {
+        'consumerKey'       : 'bC95KC69F9qmxNnlICTBfn2xs',
+        'consumerSecret'    : 'hnhZjlvaCrtHWQsUKffvhQZYpmhOIUBuRm7fH6YsYCSglKZybo',
+        'callbackURL'       : 'http://34.239.132.148:3000/auth/twitter/callback'
+    },
+    */
+    //end global
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
